61562e65b4827604c1f8a549cf06b361e6ecd8a6
张宸 <531336750@qq.com>
2018-11-23 09:41:56 +0800

    构建整体页组件架构

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HomeComponent } from './pages/home/home.component';                                                       
+import { AppComponent } from './app.component';                                                   
 
-import { ElModule } from 'element-angular'
+import { ElModule } from 'element-angular';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SearchComponent } from './search/search.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { ProductComponent } from './product/product.component';
+import { StartComponent } from './start/start.component';
+import { FooterComponent } from './footer/footer.component'
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    NavbarComponent,
+    SearchComponent,
+    CarouselComponent,
+    ProductComponent,
+    StartComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
