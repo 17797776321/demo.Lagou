1d092198f1792f32120e903aca14602ec4629858
张宸 <531336750@qq.com>
2018-11-24 17:22:21 +0800

    添加产品

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
 
 @Component({
   selector: 'app-product',
@@ -6,10 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product.component.less']
 })
 export class ProductComponent implements OnInit {
+  private products:Array<Product>
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    this.products = [
+      new Product(1,"第一个商品",1.99,3.5,"这是第一个商品，是我再学习angular入门实战是创建的",["电子产品","硬件设备"]),
+      new Product(2,"第二个商品",2.99,2.5,"这是第二个商品，是我再学习angular入门实战是创建的",["电子产品","硬件设备"]),
+      new Product(3,"第三个商品",3.99,4.5,"这是第三个商品，是我再学习angular入门实战是创建的",["硬件设备"]),
+      new Product(4,"第四个商品",4.99,1.5,"这是第四个商品，是我再学习angular入门实战是创建的",["电子产品","硬件设备"]),
+      new Product(5,"第五个商品",5.99,3.5,"这是第五个商品，是我再学习angular入门实战是创建的",["电子产品","硬件设备"]),
+      new Product(6,"第六个商品",6.99,2.5,"这是第六个商品，是我再学习angular入门实战是创建的",["电子产品","硬件设备"]),
+    ]
   }
-
+}
+export class Product {
+  constructor(
+    public id: number,
+    public title: string,
+    public price: number,
+    public rating: number,
+    public desc: string,
+    public categories: Array<string>
+  ){}
 }
